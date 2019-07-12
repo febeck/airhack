bdc9f72047738f322eaadf2c22a3a30b3a3ddd2f
Fernando Beck <fe.beck25@gmail.com>
2019-07-12 21:49:53 +0200

    backend: random assigning tasks

--- backend/taskCalculator.js
+++ backend/taskCalculator.js
@@ -1,5 +1,13 @@
-function taskCalculator(test) {
-  return test
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max)) + 1
+}
+
+function taskCalculator(batch) {
+  const { tasks, taskersCount } = batch
+  const assignedTasks = tasks.map(task => {
+    return Object.assign(task, { assignee_id: getRandomInt(taskersCount) })
+  })
+  return Object.assign(batch, { tasks: assignedTasks })
 }
 
 module.exports = {
