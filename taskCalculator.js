a9bba05a3125496b20ea3c90571c41a048cda021
Fernando Beck <fe.beck25@gmail.com>
2019-07-13 00:22:45 +0200

    fix max asignee id

--- taskCalculator.js
+++ taskCalculator.js
@@ -6,7 +6,7 @@ function taskCalculator(batch) {
   const { tasks, taskersCount } = batch
   const assignedTasks = tasks.reduce((acc, task, i) => {
     if (i < taskersCount) {
-      acc.push(Object.assign(task, { assignee_id: i + 1 }))
+      acc.push(Object.assign(task, { assignee_id: i }))
     } else {
       acc.push(task)
     }
